35e7487024e9fb0b91d894eb1d14346a17508274
ipskm_spt <ipskm@users.noreply.github.com>
2018-12-25 23:29:09 +0700

    add comment on code

--- pro.py
+++ pro.py
@@ -6,44 +6,46 @@ import multiprocessing
 import serial
 
 def get_data():
-    ser = serial.Serial('/dev/ttyUSB0', 9600)
-    value = ser.readline()
-    data = value.split(',')
+    ser = serial.Serial('/dev/ttyUSB0', 9600) #define defult serial port 
+    value = ser.readline() #read data from serial
+    data = value.split(',') #equal value to data for split with ,
     
 
 def app_gui():
-    root = Tk()
-    root.geometry('800x600')
-    root.title('Smart Home :: กลุ่มพ่อบ้านใจกล้า')
+    root = Tk() #main window
+    root.geometry('800x600') #resolution
+    root.title('Smart Home :: กลุ่มพ่อบ้านใจกล้า') #title name
     
     def msgbox():
-        contact = 'Powered by Python\nDeveloper :\nMr.Sathit Supmek \nMr.Phongsakorn Suttama'
-        messagebox.showinfo('About US',contact)
+        contact = 'Powered by Python\nDeveloper :\nMr.Sathit Supmek \nMr.Phongsakorn Suttama' #text info
+        messagebox.showinfo('About US',contact) #call message box title About us label use in contact variable
     def load_config():
-        fileload = Tk()
-        fileload.filename =  filedialog.askopenfilename(initialdir = "/",title = "Select file",filetypes = (("csv file","*.csv"),("all files","*.*")))
-        print (fileload.filename)
+        fileload = Tk() #define fileload as windows
+        fileload.filename =  filedialog.askopenfilename(initialdir = "/",title = "Select file",filetypes = (("csv file","*.csv"),("all files","*.*"))) #make find file window
+        # print (fileload.filename)
 
-    menu = Menu(root)
-    root.config(menu=menu, bg='#ffffff')
-    fileMenu = Menu(menu, tearoff=0)
-    fileMenu.add_command(label='Load Config', command=load_config)
-    fileMenu.add_command(label='Exit',command=exit)
-    menu.add_cascade(label='File', menu=fileMenu)
-    helpMenu = Menu(menu, tearoff=0)
-    helpMenu.add_command(label='About US',command=msgbox)
-    menu.add_cascade(label='Help', menu=helpMenu)
+    menu = Menu(root) # make munubar
+    root.config(menu=menu, bg='#ffffff') #config menubar to call with menu
+    fileMenu = Menu(menu, tearoff=0) #create filemenu with fix position
+    fileMenu.add_command(label='Load Config', command=load_config) #add menu to main filemenu
+    fileMenu.add_command(label='Exit',command=exit) #add menu to main filemenu
+    menu.add_cascade(label='File', menu=fileMenu) ##add menu to root window
+    helpMenu = Menu(menu, tearoff=0) #create helpmenu with fix position
+    helpMenu.add_command(label='About US',command=msgbox) #add about as menu
+    menu.add_cascade(label='Help', menu=helpMenu) #create helpmenu with fix position
 
-    Label(root, text='Smart Home Dashboard System', font='arial 20',bg='#ffffff').pack()
+    Label(root, text='Smart Home Dashboard System', font='arial 20',bg='#ffffff').pack() #main label on root screen
 
-    root.mainloop()
+
+
+    root.mainloop() #show root window
 
 if __name__ == "__main__":
-    p1 = multiprocessing.Process(target = app_gui)
-    p2 = multiprocessing.Process(target = get_data)
+    p1 = multiprocessing.Process(target = app_gui) #define multi process
+    p2 = multiprocessing.Process(target = get_data) #define multi process
 
-    p1.start()
-    p2.start()
+    p1.start() #start process
+    p2.start() #start process
 
-    p1.join()
-    p2.join()
+    p1.join() #join function
+    p2.join() #join function
